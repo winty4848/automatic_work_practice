2cb484cfdff2d1bf11cdbaa00e1021023fbf2ae2
winty4848 <winty95@nate.com>
2022-08-29 11:23:49 +0900

    4_1 실습

---
+++ ch4_excel_task/s1_get_fake_info_csv.py
@@ -0,0 +1,69 @@
+import time
+import random
+import os
+
+print("가상정보 생성 시작")
+
+# 처리시간 출력용
+start_time=time.time()
+
+# 정보 생성 개수 제어
+num_sample=1000
+
+# 메일, 메일도메인, 이름 생성용
+alphabet_list="abcdefghizklmnopqrstuvwxyz1234567890"
+domain_list=["naver","google","nate","kakao","tistory"]
+first_name_samples = "김이박최정강조윤장임"
+middle_name_samples = "민서예지도하주윤채현지"
+last_name_samples = "준윤우원호후서연아은진"
+
+# 영어글자 생성
+def random_string(length):
+    result = ""
+    for i in range(length):
+        result += random.choice(alphabet_list)
+    return result
+
+# 이름 생성
+def random_name():
+    result = ""
+    result += random.choice(first_name_samples)
+    result += random.choice(middle_name_samples)
+    result += random.choice(last_name_samples)
+    return result
+
+# 결과물 저장할 폴더 생성
+os.mkdir("personal_fake_info")
+
+# 어짜피 csv형태로 저장할꺼니까 헤더를 그냥 미리 정의한다.
+header=",".join(["name", "age", "e-mail", "telephone", "sex"])+"\n"
+
+for i in range(num_sample):
+    name=random_name()
+
+    # 결과물 파일에 이름 붙이기(F2 누른거랑 같음)
+    filename = "personal_fake_info/" + str(i) + "_" + name + ".csv"
+
+    # 파일 생성
+    outfile = open(filename, 'w')
+    
+    # 이전 예제(TXT파일 1000개 생성)과 달리 헤더를 미리 정의
+    outfile.write(header)
+    
+    outfile.write(name + ",")
+
+    # 20이상 60이하 정수를 뽑아 나이를 설정
+    # concat은 문자데이터만 되기 때문에 str()
+    outfile.write(str(random.randint(20, 60)) + ",")
+
+    # 미리 설정해둔 도메인 중에서 하나 뽑아 이메일 구성
+    outfile.write(random_string(random.randint(5, 12)) + "@" + random.choice(domain_list) + ".com,")
+
+    outfile.write("010-" + str(time.time())[-4:] + "-" + str(time.time())[-6:-2] + ",")
+    outfile.write(random.choice(["male", "female"]))
+
+    outfile.close()
+
+print("가상정보 생성 끝")
+end_time=time.time()
+print("소요시간 : " + str(end_time-start_time) + "초")
